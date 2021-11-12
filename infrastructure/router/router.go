40e4842a9f4c434784db8aef07118b789e3dfc2e
ErickRodriguezWize <erick.rodriguez@wizeline.com>
2021-11-12 06:02:24 -0800

    ADD router for project

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -1,1 +1,31 @@
 package router
+
+import (
+	"WizelineApi/interface/controller"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"fmt"
+)
+
+func NewRouter() {
+
+	
+
+	r := mux.NewRouter()
+
+	handler_pkm := controller.NewPokemonController()
+	//Pokemon Endpoints
+	r.HandleFunc("/pokemons", handler_pkm.GetAllPokemons).Methods("GET")
+	r.HandleFunc("/pokemon/{id}", handler_pkm.GetPokemon).Methods("GET")
+
+	//Server setup
+	srv := &http.Server{
+		Handler: r,
+		Addr: "127.0.0.1:8000",
+	}
+
+	fmt.Printf("Server Started at %v", srv.Addr)
+
+	log.Fatal(srv.ListenAndServe())
+}
