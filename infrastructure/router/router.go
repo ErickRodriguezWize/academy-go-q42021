489310ba8403105590fa60890548de8972225fc5
ErickRodriguezWize <erick.rodriguez@wizeline.com>
2021-11-16 16:04:52 -0800

    ADD: Environments Variables using viper

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -3,19 +3,18 @@ package router
 import (
 	"log"
 	"net/http"
-	"fmt"
 
 	"github.com/ErickRodriguezWize/academy-go-q42021/interface/controller"
+	"github.com/ErickRodriguezWize/academy-go-q42021/service"
 
 	"github.com/gorilla/mux"
 )
-
-
 
 func NewRouter() {
 	r := mux.NewRouter()
 
 	pokemonHandler := controller.NewPokemonController()
+
 	//Pokemon Endpoints
 	r.HandleFunc("/pokemons", pokemonHandler.GetAllPokemons).Methods("GET")
 	r.HandleFunc("/pokemons/{id}", pokemonHandler.GetPokemon).Methods("GET")
@@ -23,10 +22,11 @@ func NewRouter() {
 	//Server setup
 	srv := &http.Server{
 		Handler: r,
-		Addr: "127.0.0.1:8000",
+		Addr: service.GetEnvVariable("IP_ADDRESS") + ":" + service.GetEnvVariable("PORT"),
 	}
 
-	fmt.Printf("Server Started at %v", srv.Addr)
+	log.Println("Server Started at", srv.Addr)
 
 	log.Fatal(srv.ListenAndServe())
+
 }
---
+++ service/viper_service.go
@@ -0,0 +1,32 @@
+/* This service focus on reading the Environment Files of .env using viper go package.  */
+
+package service
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+func GetEnvVariable(key string) (string) {
+	//Set config filename
+	viper.SetConfigName("config")
+	//Set config file path. 
+	viper.AddConfigPath(".")
+
+	//Find and read config file
+	err:= viper.ReadInConfig()
+
+	if err != nil{
+		log.Fatal("Error: Couldn't Read or find the Environment File.")
+	}	
+
+	//This method returns value and a boolean
+	value, ok := viper.Get(key).(string)
+	if !ok{
+		log.Fatalf("Error: Couldn't find the key(%v) on the Environment Variable file.", key)
+	}
+
+	return value
+
+}
