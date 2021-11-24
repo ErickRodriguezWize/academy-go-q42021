b16b2c1e16d1632e6649bd35250c32f00f113c47
ErickRodriguezWize <erick.rodriguez@wizeline.com>
2021-11-23 17:30:46 -0800

    Update: viper service, add GetIntEnvVariable func.

--- service/viper_service.go
+++ service/viper_service.go
@@ -8,25 +8,44 @@ import (
 	"github.com/spf13/viper"
 )
 
-func GetEnvVariable(key string) (string) {
+//SetupViper: Make the configuration for the viper module (config file, define paths, read config.yaml file).
+func SetupViper() {
 	//Set config filename
 	viper.SetConfigName("config")
-	//Set config file path. 
+	//Set config file path.
 	viper.AddConfigPath(".")
 
 	//Find and read config file
-	err:= viper.ReadInConfig()
+	err := viper.ReadInConfig()
 
-	if err != nil{
+	if err != nil {
 		log.Fatal("Error: Couldn't Read or find the Environment File.")
-	}	
+	}
+}
 
-	//This method returns value and a boolean
+//GetEnvVariable: Find a String value inside config.yaml(Environment variable file).
+func GetEnvVariable(key string) string {
+	SetupViper()
+
+	//This method returns value (string) and a boolean
 	value, ok := viper.Get(key).(string)
-	if !ok{
-		log.Fatalf("Error: Couldn't find the key(%v) on the Environment Variable file.", key)
+	if !ok {
+		log.Fatalf("Error: Couldn't find the string value(%v) on the Environment Variable file.", key)
 	}
 
 	return value
 
+}
+
+//GetInvVariable: Find an Integer value on config.yaml(Environment variable file).
+func GetIntEnvVariable(key string) int {
+	SetupViper()
+
+	//This methods return value (int).
+	value := viper.GetInt(key)
+	if value == 0 {
+		log.Fatalf("Error: Couldn't find the Integer Value(%v) on the Environment Variable File.", key)
+	}
+
+	return value
 }
