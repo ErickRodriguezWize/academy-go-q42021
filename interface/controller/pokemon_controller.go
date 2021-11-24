81aab2551d0bd4124fbcbec947d0d792992e8d95
ErickRodriguezWize <erick.rodriguez@wizeline.com>
2021-11-23 17:32:15 -0800

    Fix: Comments, import accomodations and format files.

--- interface/controller/pokemon_controller.go
+++ interface/controller/pokemon_controller.go
@@ -1,10 +1,10 @@
 package controller
 
 import (
-	"net/http"
 	"encoding/json"
-	"strconv"
 	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/ErickRodriguezWize/academy-go-q42021/domain/model"
 	"github.com/ErickRodriguezWize/academy-go-q42021/service"
@@ -12,29 +12,37 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//Empty Structure
 type pokemonController struct{}
 
-type PokemonController interface{
+type PokemonController interface {
 	GetAllPokemon()
 	GetPokemon()
 }
 
-// GetAllPokemon returns all values inside the csv File. 
-func (pc *pokemonController) GetAllPokemons(res http.ResponseWriter, req *http.Request)  {
+// GetAllPokemon: returns all values inside the csv File.
+func (pc *pokemonController) GetAllPokemons(res http.ResponseWriter, req *http.Request) {
 	log.Println("HTTP GET /pokemons")
+	CsvPath := service.GetEnvVariable("CSV_PATH")
 
+	//Get an array of model.Pokemon from service "ReadCSV".
 	pokemons := []model.Pokemon{}
-	if err := service.ReadCSV("./test/bateria_csv/pokemon.csv", &pokemons); err != nil {
-		log.Println("Error: " + err.Error() )
+	if err := service.ReadCSV(CsvPath, &pokemons); err != nil {
+		log.Println("Error: " + err.Error())
 		http.Error(res, err.Error(), http.StatusBadRequest)
-		
+
 		return
 	}
 
+	//Setup response (headers, http Status)
 	res.Header().Set("Content-Type", "application/json")
 	res.WriteHeader(http.StatusOK)
-	json.NewEncoder(res).Encode(pokemons)
+
+	//Handling Response Json.
+	if err := json.NewEncoder(res).Encode(pokemons); err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+
+		return
+	}
 
 }
 
@@ -42,40 +50,51 @@ func (pc *pokemonController) GetAllPokemons(res http.ResponseWriter, req *http.R
 func (pc *pokemonController) GetPokemon(res http.ResponseWriter, req *http.Request) {
 	pkm := model.Pokemon{}
 	params := mux.Vars(req)
-	log.Printf("HTTP GET /pokemons/%v \n",params["id"])
+	CsvPath := service.GetEnvVariable("CSV_PATH")
+	log.Printf("HTTP GET /pokemons/%v \n", params["id"])
 
-	id,err := strconv.Atoi(params["id"])
+	//Parsing the 'id' from string into int.
+	id, err := strconv.Atoi(params["id"])
 	if err != nil {
 		log.Println("ERROR: Couldn't parse Properly to Integer.")
 		http.Error(res, err.Error(), http.StatusBadRequest)
-		
+
 		return
 	}
 
+	//Get an array of model.Pokemon from service "ReadCSV".
 	pokemons := []model.Pokemon{}
-	if err := service.ReadCSV("./test/bateria_csv/pokemon.csv", &pokemons); err != nil {
-		log.Println("Error:"+err.Error())
+	if err := service.ReadCSV(CsvPath, &pokemons); err != nil {
+		log.Println("Error:" + err.Error())
 		http.Error(res, err.Error(), http.StatusBadRequest)
 
 		return
-	}	
+	}
 
+	//Find a pokemon using ID as a filter.
 	pkm, err = service.GetPokemonByID(pokemons, id)
 	if err != nil {
-		log.Printf("ERROR: Couldn't find pokemon with id: %v \n",id)
+		log.Printf("ERROR: Couldn't find pokemon with id: %v \n", id)
 		http.Error(res, err.Error(), http.StatusBadRequest)
-		
+
 		return
 	}
-	
+
+	//Setup response (headers, http Status)
 	res.Header().Set("Content-Type", "application/json")
 	res.WriteHeader(http.StatusOK)
-	json.NewEncoder(res).Encode(pkm)
+
+	//Handling Response Json.
+	if err := json.NewEncoder(res).Encode(pkm); err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+
+		return
+	}
 
 }
 
-//Func use to get an empty structure of pokemonController. 
-func NewPokemonController() *pokemonController{
+//NewPokemonController: Returns an empty Struct of pokemonController.
+func NewPokemonController() *pokemonController {
 	return &pokemonController{}
 
 }
--- service/pokemon_service.go
+++ service/pokemon_service.go
@@ -6,25 +6,17 @@ import (
 	"github.com/ErickRodriguezWize/academy-go-q42021/domain/model"
 )
 
-var (
-	PokemonMissingError = errors.New("Couldn't find pokemon")
-)
-
-//GetPokemonById finds a pokemon inside pokemons array using the ID as filter. 
-func GetPokemonByID(pokemons []model.Pokemon, ID int) (model.Pokemon, error){
+//GetPokemonById: finds a pokemon inside pokemons array using the ID as filter.
+func GetPokemonByID(pokemons []model.Pokemon, ID int) (model.Pokemon, error) {
+	var PokemonMissingError = errors.New("Couldn't find pokemon")
 	targetPkm := model.Pokemon{}
 
-	for _,pkm := range pokemons{
+	for _, pkm := range pokemons {
 		if pkm.ID == ID {
-			targetPkm = pkm
-			break
+			return pkm, nil
 		}
 	}
 
-	if targetPkm.Name == "" {
-		return targetPkm, PokemonMissingError
-	}
-
-	return targetPkm, nil
+	return targetPkm, PokemonMissingError
 
 }
